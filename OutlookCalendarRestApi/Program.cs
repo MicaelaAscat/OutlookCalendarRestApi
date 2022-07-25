83936981e7ad58fc2802c4ea9cc2b30390642720
MicaelaAscat <micaela.ascat@gmail.com>
2022-07-25 18:20:27 -0300

    Configure authentication settings
    Use Microsoft Graph sdk
    Use Microsoft Identity platform
    Configure swagger auth

--- OutlookCalendarRestApi/Program.cs
+++ OutlookCalendarRestApi/Program.cs
@@ -1,36 +1,46 @@
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Web;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+string[] initialScopes = builder.Configuration.GetValue<string>("DownstreamApi:Scopes")?.Split(' ') ?? Array.Empty<string>();
+
 // Add services to the container.
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"))
+    .EnableTokenAcquisitionToCallDownstreamApi(options =>
+    {
+        builder.Configuration.Bind("AzureAd", options);
+    })
+    .AddMicrosoftGraph(options =>
+    {
+        options.Scopes = string.Join(' ', initialScopes);
+    })
+    .AddInMemoryTokenCaches();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddSwaggerGen(o =>
 {
     // Define that the API requires OAuth 2 tokens
-    o.AddSecurityDefinition((string)(string)"oauth2", (OpenApiSecurityScheme)(OpenApiSecurityScheme)new OpenApiSecurityScheme
+    o.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
     {
         Type = SecuritySchemeType.OAuth2,
         Flows = new OpenApiOAuthFlows
         {
             Implicit = new OpenApiOAuthFlow
             {
-                AuthorizationUrl = GetAuthorizationUrl(builder),
-                TokenUrl = GetTokenUrl(builder),
-                Scopes = new Dictionary<string, string>() {
-                    {$"{string.Format("api://{0}", builder.Configuration["AzureAd:ClientId"])}/{builder.Configuration[(string)(string)"AzureAd:Scopes"]}","" }
-                }
+                AuthorizationUrl = new Uri(string.Format("https://login.microsoftonline.com/{0}/oauth2/v2.0/authorize", builder.Configuration["AzureAd:TenantId"])),
+                TokenUrl = new Uri(string.Format("https://login.microsoftonline.com/{0}/oauth2/v2.0/token", builder.Configuration["AzureAd:TenantId"])),
+                Scopes = builder.Configuration["AzureAd:Scopes"]
+                                .Split(' ')
+                                .ToDictionary(s => s.Trim(), s => ""),
             }
         }
-    });
-    SwaggerGenOptionsExtensions.AddSecurityRequirement(o, (OpenApiSecurityRequirement)new OpenApiSecurityRequirement() {
+    }); ;
+    SwaggerGenOptionsExtensions.AddSecurityRequirement(o, new OpenApiSecurityRequirement() {
     {
         new OpenApiSecurityScheme {
             Reference = new OpenApiReference {
@@ -45,16 +55,6 @@ builder.Services.AddSwaggerGen(o =>
     }
     });
 });
-
-Uri GetAuthorizationUrl(WebApplicationBuilder builder)
-{
-    return new Uri(string.Format("https://login.microsoftonline.com/{0}/oauth2/v2.0/authorize", builder.Configuration["AzureAd:TenantId"]));
-}
-
-Uri GetTokenUrl(WebApplicationBuilder builder)
-{
-    return new Uri((string)string.Format("https://login.microsoftonline.com/{0}/oauth2/v2.0/authorize", builder.Configuration["AzureAd:TenantId"]));
-}
 
 var app = builder.Build();
 
