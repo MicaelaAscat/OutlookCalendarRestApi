2890ff0a92475eaaf9954a9f8a305e798763e185
MicaelaAscat <micaela.ascat@gmail.com>
2022-07-29 22:13:56 -0300

    Enable Swagger in azure

--- CodaCalendarRestApi/Startup.cs
+++ CodaCalendarRestApi/Startup.cs
@@ -83,17 +83,14 @@ namespace CodaCalendarRestApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c =>
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
                 {
                     c.SwaggerEndpoint("/swagger/v1/swagger.json", "CodaCalendarRestApi v1");
                     c.OAuthClientId(Configuration["AzureAd:ClientId"]);
                 });
-            }
-
+   
             app.UseHttpsRedirection();
 
             app.UseRouting();
